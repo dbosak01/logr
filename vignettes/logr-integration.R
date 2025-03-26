af59c9a8a7806446e8c417f22c848111bfbcd6b6
David J. Bosak <dbosak01@gmail.com>
2025-03-26 07:31:50 -0400

    Small documentation fixes.

--- vignettes/logr-integration.R
+++ vignettes/logr-integration.R
@@ -8,13 +8,14 @@ knitr::opts_chunk$set(
 #  #' @title Read an RDS file
 #  #' @param file_path The path to the file.
 #  #' @return The RDS file contents as an R object.
-#  #' @import logr
 #  #' @export
 #  read_file <- function(file_path) {
 #  
 #    ret <-  readRDS(file_path)
 #  
-#    log_hook(paste0("Read RDS file from '", file_path, "'"))
+#    if (length(find.package('logr', quiet=TRUE)) > 0) {
+#      logr::log_hook(paste0("Read RDS file from '", file_path, "'"))
+#    }
 #  
 #    return(ret)
 #  }
